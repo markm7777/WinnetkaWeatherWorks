069ba8243eab037ee50e57238d1be8809388113b
markm7777 <mark.murphy7777@gmail.com>
2020-03-30 06:44:29 -0700

    Updates

---
+++ precache-manifest.fd0ed70ef6949b6a1b3541ffadf0baaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d19c3c3e380f56ec5505bcb4e8f8615",
+    "url": "/WinnetkaWeatherWorks/index.html"
+  },
+  {
+    "revision": "e64725154f539519cabb",
+    "url": "/WinnetkaWeatherWorks/static/css/main.dc66d04b.chunk.css"
+  },
+  {
+    "revision": "eb6d0b1931c063752f66",
+    "url": "/WinnetkaWeatherWorks/static/js/2.bce36179.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/WinnetkaWeatherWorks/static/js/2.bce36179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e64725154f539519cabb",
+    "url": "/WinnetkaWeatherWorks/static/js/main.c566e029.chunk.js"
+  },
+  {
+    "revision": "5dc9486d70c6d6400dfe",
+    "url": "/WinnetkaWeatherWorks/static/js/runtime-main.29253637.js"
+  },
+  {
+    "revision": "099293100781957054618df7dda98b00",
+    "url": "/WinnetkaWeatherWorks/static/media/clouds2.09929310.png"
+  }
+]);
